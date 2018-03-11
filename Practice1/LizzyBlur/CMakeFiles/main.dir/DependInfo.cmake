51f8704fca57f426b5bce7536951c5b6227808ee
lizzyt10h <lizzyt10h@gmail.com>
2018-03-10 22:08:19 -0500

    Own try of blurring

--- Practice1/LizzyBlur/CMakeFiles/main.dir/DependInfo.cmake
+++ Practice1/LizzyBlur/CMakeFiles/main.dir/DependInfo.cmake
@@ -4,13 +4,14 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/nicolas/Semestre VIII/Computación Paralela y Distribuida/Practices/Practice1/LizzyBlur/main.cpp" "/home/nicolas/Semestre VIII/Computación Paralela y Distribuida/Practices/Practice1/LizzyBlur/CMakeFiles/main.dir/main.cpp.o"
+  "/home/lizzyt10h/6to semestre/Paralela y Distribuida/shared/Practice1/LizzyBlur/main.cpp" "/home/lizzyt10h/6to semestre/Paralela y Distribuida/shared/Practice1/LizzyBlur/CMakeFiles/main.dir/main.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
-  "/usr/include/opencv"
+  "/usr/local/include"
+  "/usr/local/include/opencv"
   )
 
 # Targets to which this target links.
--- Practice1/LizzyBlur/main.cpp
+++ Practice1/LizzyBlur/main.cpp
@@ -6,35 +6,34 @@ using namespace std;
 Mat Blur(Mat img) {
 	printf("Blublublubluur!\n");
 	int red, green, blue, i, j;
+	printf("img.rows %d, img.cols %d\n", img.rows, img.cols);
+	Mat mred(img.rows-4, img.cols-4, CV_8UC3, Scalar(255,255,255));
 
-	Mat mred(img.rows-2, img.cols-2, CV_8UC3,Scalar(255,255,255));
 
-	for(int x=0;x<img.rows-1;x++){
-		for(int y=0;y<img.cols-1;y++){
+
+	for(int x=0;x<img.rows-3;x++){
+		for(int y=0;y<img.cols-3;y++){
 			red = 0;
 			green = 0;
 			blue = 0;
-			Vec3b color = img.at<Vec3b>(x,y);
-
-			for(i=0; i<3; i++){
-				for(j=0; j<3; j++){
+			for(i=0; i<5; i++){
+				for(j=0; j<5; j++){
 					Vec3b color = img.at<Vec3b>(x,y);
 					red += (int) color(0);
 					green += (int) color(1);
 					blue += (int) color(2);
 				}
 			}
-			red=red/9;
-			green=green/9;
-			blue=blue/9;
 			//printf("r:%d, g:%d, b:%d\n", red, green, blue);
-			mred.at<Vec3b>(x,y)[0] = red;
-			mred.at<Vec3b>(x,y)[1] = green;
-			mred.at<Vec3b>(x,y)[2] = blue;
+			mred.at<Vec3b>(x,y) = Vec3b(red/25,green/25,blue/25);
 		}
 	}
 
-	return mred;
+	imwrite( "blurred.jpg", mred);
+	printf("mred, r:%d, c:%d\n", mred.rows, mred.cols);
+	namedWindow( "Blurred", CV_WINDOW_AUTOSIZE );
+	imshow( "Blurred", mred );
+	waitKey(0);
 }
 
 int main(int argc, char** argv ){
@@ -44,36 +43,20 @@ int main(int argc, char** argv ){
         return -1;
     }
 
-	Mat img;
+	Mat image;
+	image = imread( argv[1], 1 );
 
-    img = imread( argv[1], 1 );
-
-	if ( !img.data ){
+	if ( !image.data ){
         printf("No image data \n");
         return -1;
-    }
-
-    //Un Mat funciona como un array en python,
-    //cuando uno lo manda a una función, la
-    //función altera el Mat original y no a una copia.
-		Mat blurred;
-
-		for(int x = 0; x < 100 ; x++){
-			blurred = Blur(img);
-		}
-
-		printf("img, r:%d, c:%d\n", img.rows, img.cols);
-		printf("mred, r:%d, c:%d\n", blurred.rows, blurred.cols);
-
-			imwrite( "blurred.jpg", blurred);
+  }
+	Blur(image);
 
 
-			namedWindow( "Original", CV_WINDOW_AUTOSIZE );
-			namedWindow( "Blurred", CV_WINDOW_AUTOSIZE );
+	printf("img, r:%d, c:%d\n", image.rows, image.cols);
 
-			imshow( "Original", img );
-			imshow( "Blurred", blurred );
-			waitKey(0);
+
+
 
 
 

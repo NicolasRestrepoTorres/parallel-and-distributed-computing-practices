23e4aa656cf7adb3fe9046031f4975145e0f0141
lizzyt10h <lizzyt10h@gmail.com>
2018-03-16 12:20:51 -0500

    Put content of getAverageAround inside BoxBlur function and change pthread_create argument

--- Practice1/posix_blur/blur-effect2.cpp
+++ Practice1/posix_blur/blur-effect2.cpp
@@ -24,58 +24,56 @@ bool checkBounds(int a, int b, int bx, int by){
 
 int *getAverageAround(int a, int b){
 
+	int accumulator, cornerX, cornerY, sumRGB[] = {0,0,0};
+	static int arr[3];
+	accumulator=0;
+	if(k%2==0){
+		cornerX = a-(k-1)/2;
+		cornerY = b-(k-1)/2;
+	}else{
+		cornerX = a-(k-2)/2;
+		cornerY = b-(k-2)/2;
+	}
 
-	//return arr;
+	for(int i = 0; i < k; i++){
+		for(int j = 0; j < k; j++){
+			if(checkBounds(cornerX+j,cornerY+i, img.cols, img.rows)){
+				Vec3b color = img.at<Vec3b>(Point(cornerX+j,cornerY+i));
+				sumRGB[0] += (int) color(0);
+				sumRGB[1] += (int) color(1);
+				sumRGB[2] += (int) color(2);
+				accumulator++;
+			}
+		}
+	}
+
+	if(accumulator == 0){
+		arr[0] = img.at<Vec3b>(Point(a,b))(0);
+		arr[1] = img.at<Vec3b>(Point(a,b))(1);
+		arr[2] = img.at<Vec3b>(Point(a,b))(2);
+	}else{
+		arr[0] = sumRGB[0] / accumulator;
+		arr[1] = sumRGB[1] / accumulator;
+		arr[2] = sumRGB[2] / accumulator;
+	}
+	return arr;
 }
 
 
 void *BoxBlur(void*ap){
-	int id = *((int *) ap);
-	free(ap);
-	int accumulator, cornerX, cornerY, sumRGB[] = {0,0,0};
-	//printf("Hilo: %d\n", id);
+	int id;
+	id = *((int *) ap);
+	id--;
+
 	for(int x=id;x<img.cols;x+=THREADS){
-		//printf("hilo: %d, col: %d\n", id, x);
 		for(int y=0;y<img.rows;y++){
-			//sem_wait(&semvar2);
-			sumRGB[0]=0;
-			sumRGB[1]=0;
-			sumRGB[2]=0;
-			accumulator=0;
-			if(k%2==0){
-				cornerX = x-(k-1)/2;
-				cornerY = y-(k-1)/2;
-			}else{
-				cornerX = x-(k-2)/2;
-				cornerY = y-(k-2)/2;
-			}
 
-			for(int i = 0; i < k; i++){
-				for(int j = 0; j < k; j++){
-					if(checkBounds(cornerX+j,cornerY+i, img.cols, img.rows)){
-						Vec3b color = img.at<Vec3b>(Point(cornerX+j,cornerY+i));
-						sumRGB[0] += (int) color(0);
-						sumRGB[1] += (int) color(1);
-						sumRGB[2] += (int) color(2);
-						accumulator++;
-					}
-				}
-			}
+			int *results = getAverageAround(x, y);
 
-			if(accumulator == 0){
-				sumRGB[0] = img.at<Vec3b>(Point(x,y))(0);
-				sumRGB[1] = img.at<Vec3b>(Point(x,y))(1);
-				sumRGB[2] = img.at<Vec3b>(Point(x,y))(2);
-			}else{
-				sumRGB[0] = sumRGB[0] / accumulator;
-				sumRGB[1] = sumRGB[1] / accumulator;
-				sumRGB[2] = sumRGB[2] / accumulator;
-			}
-			//sem_post(&semvar2);
 			Vec3b color;
-			color(0) = sumRGB[0];
-			color(1) = sumRGB[1];
-			color(2) = sumRGB[2];
+			color(0) = results[0];
+			color(1) = results[1];
+			color(2) = results[2];
 			sem_wait(&semvar);
 				blurred.at<Vec3b>(Point(x,y)) = color;
 			sem_post(&semvar);
@@ -88,7 +86,7 @@ int main(int argc, char** argv ){
 		clock_t start, end;
 		sem_init(&semvar, 0, 1);
 		sem_init(&semvar2, 0, 1);
-		int r;
+		int r, t;
 
 		start=clock();
 		gettimeofday(&tv1, NULL);
@@ -105,9 +103,7 @@ int main(int argc, char** argv ){
 		blurred = Mat(img.rows, img.cols, CV_8UC3, Scalar(255,255,255));
 
 		for(int t=0; t<THREADS; t++){
-			int *arg = (int*) malloc(sizeof(*arg));
-			*arg = t;
-			r = pthread_create(&hilo[t], NULL, BoxBlur, arg);
+			r = pthread_create(&hilo[t], NULL, &BoxBlur, (void*)&t);
 			if(r != 0){
 				perror("Error en pthread_create");
 				exit(-1);
@@ -121,6 +117,8 @@ int main(int argc, char** argv ){
 				exit(-1);
 			}
 		}
+
+
 
 		imwrite( argv[2], blurred );
 
